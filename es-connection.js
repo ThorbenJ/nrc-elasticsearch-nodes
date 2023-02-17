6fbd0ed15c2b14f61e6616ab4f0bc28812ae2c37
Thorben Jändling <thorbenj@users.noreply.github.com>
2023-02-17 18:42:44 +0100

    better cred handling

--- es-connection.js
+++ es-connection.js
@@ -3,71 +3,69 @@ module.exports = function (RED) {
     
     function EsConnectionNode(n) {
         RED.nodes.createNode(this, n);
-        this.nodes = n.nodes;
-        this.reqtimeout = n.reqtimeout;
-        this.proxy = n.proxy;
-
-//         avoid if nesting
-        do {
-            
-            var r = n.auth.match(/^username: ([^;]+); password: (.+)/);
-            if (r && r.length == 3) {
-                this.auth = {
-                    username: r[1],
-                    password: r[2]
+        this.conf = n
+        var auth;
+        
+        switch (this.credentials.cred){
+            case "":
+                auth = null;
+                break;
+            case "basic":
+                auth = {
+                    username: this.credentials.ident,
+                    password: this.credentials.secret
                 };
                 break;
-            }
-
-            r = n.auth.match(/^apiKey: (.+)/);
-            if (r && r.length == 2) {
-                this.auth = {
-                    apiKey: r[1]
+            case "apikey_b64":
+                auth = {
+                    apiKey: this.credentials.secret
                 };
                 break;
-            }
-
-            r = n.auth.match(/^api_id: ([^;]+); api_key: (.+)/);
-            if (r && r.length == 3) {
-                this.auth = {
+            case "apikey_obj":
+                auth = {
                     apiKey: {
-                        id: r[1],
-                        api_key: r[2]
+                        id: this.credentials.ident,
+                        api_key: this.credentials.secret
                     }
                 };
                 break;
-            }
-
-            r = n.auth.match(/^bearer: (.+)/);
-            if (r && r.length == 2) {
-                this.auth = {
-                    bearer: r[1]
+            case "bearer":
+                auth = {
+                    bearer: this.credentials.secret
                 };
                 break;
-            }
-
-            bob.auth = null;
-        } while (false);
+            default:
+                //TODO error
+                console.log("HERE BE DRAGONS!");
+        }
         
         this.client = function(c){
-            if (this._conn) { return this._conn.child(c) };
+            if (this._conn)
+                return this._conn.child(c);
             
             var params = {
-                nodes: this.nodes.split(' ')            }
+                nodes: this.conf.nodes.split(' ')
+            };
             
-            if (this.requestTimeout && this.requestTimeout > 0)
-                params.requestTimeout = this.requestTimeout;
+            if (this.conf.requestTimeout && this.conf.requestTimeout > 0)
+                params.requestTimeout = this.conf.requestTimeout;
             
-            if (this.auth && this.auth !== '')
-                params.auth = this.auth;
+            if (auth && auth !== '')
+                params.auth = auth;
             
-            if (this.proxy && this.proxy !== '')
-                params.proxy = this.proxy;
+            if (this.conf.proxy && this.conf.proxy !== '')
+                params.proxy = this.conf.proxy;
             
             this._conn = new Client(params);
             
             return this._conn.child(c);
         }
     }
-    RED.nodes.registerType("es-connection", EsConnectionNode);
+    RED.nodes.registerType("es-connection", EsConnectionNode, {
+        credentials: {
+            cred: {type: "text"},
+            ident: {type:"text"},
+            secret: {type:"password"}
+        }
+    });
 };
